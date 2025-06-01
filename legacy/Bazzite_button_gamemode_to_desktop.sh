968712ebb64dc3883fa690813012737a0999770e
w0lfwang <w0lfwang@hotmail.com>
2025-06-01 00:12:02 -0500

    For legacy, my first working attempt

---
+++ legacy/Bazzite_button_gamemode_to_desktop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if systemctl is-active gdm.service --quiet; then
+    echo "Desktopmode is already running."
+    sleep 2
+    exit 0
+fi
+
+echo "Starting system-wide switch service..."
+
+if ! sudo systemctl start bazzite_switch_to_desktop.service; then
+    echo "Failed to start the switch-to-sddm service. Exiting without logout."
+    exit 1
+fi
